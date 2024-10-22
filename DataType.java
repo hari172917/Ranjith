2bf8ef7318ed4c6cea47992c0301d3ee43fdc7b8
hari172917 <hariguna1729@gmail.com>
2024-10-22 22:25:21 +0530

    f

--- DataType.java
+++ DataType.java
@@ -2,7 +2,13 @@ class DataType {
     public static void main(String[] args) {
         byte a=-128; //range from -128 to 127
         boolean b=false;
-        System.out.println(b);
+        System.out.println(3==4);
         System.out.println(a);
+        String f=new String("Hi");
+        String f=new String("Hi");
+        System.out.println(f1==f2);
+        System.out.println("Hi".equals("Hi"));
+
     }
 }
+ 
