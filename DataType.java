82559bda35d7196352331aa1f6f7e72a00b19dff
hari172917 <hariguna1729@gmail.com>
2024-10-22 22:10:17 +0530

    f

---
+++ DataType.java
@@ -0,0 +1,8 @@
+class DataType {
+    public static void main(String[] args) {
+        byte a=-128; //range from -128 to 127
+        boolean b=false;
+        System.out.println(b);
+        System.out.println(a);
+    }
+}
